8468d7e350d12f8c29509bebadfb4c3c765d0a57
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2024-08-28 17:37:03 +0800

    changed title limit

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,6 +1,6 @@
-<a href="{{ route('movie.show', $movie['id']) }}" class="card text-decoration-none" style="width: 18rem;">
+<a href="{{ route('movie.show', $movie['id']) }}" class="card movie text-decoration-none" style="width: 18rem;">
     <img src="{{ 'https://images.tmdb.org/t/p/w500'.$movie['poster_path'] }}" class="card-img-top" alt="{{ $movie['original_title'] }}">
-    <div class="card-body">
+    <div class="card-body d-flex justify-content-between flex-column">
         <h3 class="movie-title">{{ $movie['original_title'] }}</h3>
         <p class="card-text movie-descriptionn">{{ $movie['overview'] }}</p>
         <div class="movie-rate">
